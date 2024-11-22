794a24725fd3ac69c44f1aa402742e417bf0be1a
boris sigma <1322553126@qq.com>
2024-11-22 15:54:41 +0800

    Update terminal.js

--- components/apps/terminal.js
+++ components/apps/terminal.js
@@ -206,8 +206,10 @@ export class Terminal extends Component {
         let rest = words.join(" ");
         rest = rest.trim();
         switch (main) {
-            default:
-                result = await callOpenAI(command, this.state.image);
+            case "main library":
+                result = "Follow the main road straight ahead, and you'll come across the Faculty of Engineering. Then, turn left and walk approximately 10 meters straight. After that, there's a downhill slope. Once you've walked down the slope, the Main Library will be on your right.";
+            // default:
+            //     result = await callOpenAI(command, this.state.image);
         }
         document.getElementById(`row-result-${rowId}`).innerHTML = result;
         this.appendTerminalRow();
